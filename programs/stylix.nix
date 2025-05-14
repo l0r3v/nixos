f6aea39050af3c910901ce1f90e4378ba4b7743b
lorev <lorenzopasqui@gmail.com>
2025-05-14 15:47:48 +0200

    changed wallpaper

--- programs/stylix.nix
+++ programs/stylix.nix
@@ -6,24 +6,23 @@
   inputImage = ../config/wallpaper/nix-transp.png;
   # https://tinted-theming.github.io/tinted-gallery/
   theme = "${pkgs.base16-schemes}/share/themes/${themeName}.yaml";
-  wallpaper = pkgs.runCommand "nix-colored.png" {} ''
+  wallpaper-nix = pkgs.runCommand "nix-colored.png" {} ''
     COLOR=$(${pkgs.yq}/bin/yq -r .palette.base00 ${theme})
     ${pkgs.imagemagick}/bin/convert -background $COLOR -flatten ${inputImage} $out
   '';
+    wallpaper-wing = ../config/wallpaper/wingInFlux.jpg;
 in {
   stylix = {
     enable = true;
     base16Scheme = theme;
-    image = wallpaper;
+    image = wallpaper-wing;
     opacity = {
-      applications = 0.1; #This doesn't seem to work
-      desktop = 0.75;
-      popups = 0.85;
+      applications = 1.0; #This doesn't seem to work
+      desktop = 0.95;
+      popups = 0.95;
       terminal = 0.95;
     };
     polarity = "dark";
-    targets.floorp.profileNames = ["lorev"];
-    targets.floorp.colorTheme.enable = true;
 
     cursor = {
       package = pkgs.whitesur-cursors;
