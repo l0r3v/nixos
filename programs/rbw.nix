ea5ecea66687d9422b77ae27f146355dbd6e3025
lorev <lorenzopasqui@gmail.com>
2025-05-14 15:45:56 +0200

    added rbw

---
+++ programs/rbw.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  programs.rbw = {
+    enable = true;
+    settings = {
+      email = "lorenzopasqui@gmail.com";
+      base_url = "https://vault.pasqui.casa";
+      pinentry = pkgs.pinentry-gnome3;
+    };
+  };
+}
