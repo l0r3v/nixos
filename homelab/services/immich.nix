b4ff626a8bb0a771633ecb96a0021c9a87a28ff3
lorev <lorenzopasqui@gmail.com>
2025-10-18 14:54:33 +0200

    added nginx in front of immich to fix cors error

--- homelab/services/immich.nix
+++ homelab/services/immich.nix
@@ -7,7 +7,37 @@
     DB_PASSWORD=${config.sops.placeholder."immich/db_password"}
     POSTGRES_PASSWORD=${config.sops.placeholder."immich/db_password"}
   '';
-
+  services.nginx = {
+    enable = true;
+    virtualHosts."immich-cors" = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 2285;
+        }
+      ];
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:2283";
+        extraConfig = ''
+          if ($request_method = 'OPTIONS') {
+            add_header 'Access-Control-Allow-Origin' '*' always;
+            add_header 'Access-Control-Allow-Methods' 'GET, PUT, POST, DELETE, OPTIONS' always;
+            add_header 'Access-Control-Allow-Headers' 'X-Api-Key, User-Agent, Content-Type' always;
+            add_header 'Access-Control-Max-Age' 1728000;
+            add_header 'Content-Type' 'text/plain charset=UTF-8';
+            add_header 'Content-Length' 0;
+            return 204;
+          }
+          if ($request_method ~* '(GET|POST)') {
+              add_header 'Access-Control-Allow-Origin' '*' always;
+              add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, DELETE, OPTIONS' always;
+              add_header 'Access-Control-Allow-Headers' 'X-Api-Key, User-Agent, Content-Type' always;
+              add_header 'Access-Control-Max-Age' 1728000;
+          }
+        '';
+      };
+    };
+  };
   services.immich = {
     enable = true;
     port = 2283;
