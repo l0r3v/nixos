16eb170eb9fa6fe92d7e38794cba3eff6fefe76b
lorev <lorenzopasqui@gmail.com>
2025-10-26 17:37:05 +0100

    added cachix niri

--- XPSnixos/configuration.nix
+++ XPSnixos/configuration.nix
@@ -51,9 +51,11 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://nix-community.cachix.org"
+      "https://niri.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
