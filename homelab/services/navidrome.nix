f28637dd81f9b7347e2d9ed60da72c1bef453cc9
lorev <lorenzopasqui@gmail.com>
2025-11-10 23:07:25 +0100

    modified music folder

--- homelab/services/navidrome.nix
+++ homelab/services/navidrome.nix
@@ -2,8 +2,13 @@
   services.navidrome = {
     enable = true;
     settings = {
-      MusicFolder = "/srv/archive/navidrome/music";
+      MusicFolder = "/srv/archive/music";
       DataFolder = "/srv/archive/navidrome/config";
     };
+  };
+  users.groups.music = {};
+
+  users.users = {
+    navidrome.extraGroups = ["music"];
   };
 }
