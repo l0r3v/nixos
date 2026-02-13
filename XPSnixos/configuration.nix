36ac1383f0356f348ced6c793282a8596586d3fc
lorev <lorenzopasqui@gmail.com>
2026-02-13 16:18:27 +0100

    feat(xps): added nixbuild.net support

--- XPSnixos/configuration.nix
+++ XPSnixos/configuration.nix
@@ -52,7 +52,32 @@
       "owncloud"
     ];
   };
+  programs.ssh.extraConfig = ''
+    Host eu.nixbuild.net
+    PubkeyAcceptedKeyTypes ssh-ed25519
+    ServerAliveInterval 60
+    IPQoS throughput
+    IdentityFile /home/lorev/.ssh/my-nixbuild-key
+  '';
 
+  programs.ssh.knownHosts = {
+    nixbuild = {
+      hostNames = ["eu.nixbuild.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+    };
+  };
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+    ];
+  };
   boot = {
     resumeDevice = "/dev/disk/by-uuid/fdc651ed-f77f-4e32-98eb-a24a7a021853";
     kernelParams = [
