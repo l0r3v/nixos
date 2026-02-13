be3e7f36353b6b11148f20805e632f974edd5b45
lorev <lorenzopasqui@gmail.com>
2026-02-13 16:53:43 +0100

    feat(homelab): added nixbuild.net support

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -28,6 +28,32 @@ in {
       nvidiaSettings = false;
     };
   };
+  programs.ssh.extraConfig = ''
+    Host eu.nixbuild.net
+    PubkeyAcceptedKeyTypes ssh-ed25519
+    ServerAliveInterval 60
+    IPQoS throughput
+    IdentityFile /home/hspasqui/.ssh/nixbuild-key
+  '';
+
+  programs.ssh.knownHosts = {
+    nixbuild = {
+      hostNames = ["eu.nixbuild.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+    };
+  };
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = ["benchmark" "big-parallel"];
+      }
+    ];
+  };
+
   swapDevices = [
     {
       device = "/var/lib/swapfile";
