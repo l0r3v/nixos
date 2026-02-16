e4e2bce5b63b1ad989fd40dc084215613aae16d7
lorev <lorenzopasqui@gmail.com>
2026-02-17 00:41:20 +0100

    fix(haos): removed network configuration and given dongle control

--- homelab/vm/haos/install-script.sh
+++ homelab/vm/haos/install-script.sh
@@ -3,7 +3,7 @@ set -e
 virt-install \
     --name homeassistant \
     --description "Home Assistant OS" \
-    --os-variant=haos \
+    --os-variant=generic \
     --memory 3072 \
     --boot uefi \
     --disk /srv/archive/VMs/haos_ova-15.1.qcow2,bus=scsi \
@@ -12,6 +12,7 @@ virt-install \
     --vcpus=2 \
     --graphics none \
     --network none \
+    --hostdev 10c4:ea60 \
     --hostdev 0b95:1790 \
     --check path_in_use=off
 #virt-install --name haos --description "Home Assistant OS" --os-variant=generic --ram=4096 --vcpus=2 --disk <PATH TO QCOW2 FILE>,bus=scsi --controller type=scsi,model=virtio-scsi --import --graphics none --boot uefi
