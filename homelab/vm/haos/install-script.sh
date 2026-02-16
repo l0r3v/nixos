49f76aab897b45cb545a44fc508baa87f6466a87
lorev <lorenzopasqui@gmail.com>
2026-02-17 00:26:47 +0100

    feat(haos): added device ethernet dongle

--- homelab/vm/haos/install-script.sh
+++ homelab/vm/haos/install-script.sh
@@ -1,13 +1,17 @@
 set -e
 
 virt-install \
-    --name homeassistant\
-    --description "Home Assistant OS" --os-variant=generic \
+    --name homeassistant \
+    --description "Home Assistant OS" \
+    --os-variant=haos \
     --memory 3072 \
     --boot uefi \
-    --disk /srv/archive/VMs/haos_ova-15.1.qcow2,bus=scsi --controller type=scsi,model=virtio-scsi --import\
+    --disk /srv/archive/VMs/haos_ova-15.1.qcow2,bus=scsi \
+    --controller type=scsi,model=virtio-scsi \
+    --import \
     --vcpus=2 \
-    --graphics none\
-    --network network=macvtap-net\
+    --graphics none \
+    --network none \
+    --hostdev 0b95:1790 \
     --check path_in_use=off
 #virt-install --name haos --description "Home Assistant OS" --os-variant=generic --ram=4096 --vcpus=2 --disk <PATH TO QCOW2 FILE>,bus=scsi --controller type=scsi,model=virtio-scsi --import --graphics none --boot uefi
